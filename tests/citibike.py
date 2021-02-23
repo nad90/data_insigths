9ded0eb7059214202c8ba9671ef76632068a9e7b
unknown <nadiath.waidi@gmail.com>
2021-02-23 23:11:11 +0100

    citibike_test geo

--- tests/citibike.py
+++ tests/citibike.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 from config import dir_infos
 from config import db_infos
+import os
 
 db_user = db_infos.DB_USER
 db_pass = db_infos.DB_PASSWORD
@@ -45,15 +46,15 @@ for file in list_files:
     print(file, i,"/", len(list_files))
     df_el=pd.read_csv(tst_data_dir + file)
     df_el.columns=columns_list
-    df_el.head(10000).to_sql(table_name, mysql_engine, index=False, if_exists="append")
-"""
+    df_el.to_sql(table_name, mysql_engine, index=False, if_exists="append")
+
 print("GETTING INSIGHTS THROUGH SQL")
 query_user = "SELECT year, user_age_at_trip, gender, COUNT(1)  as number_trips " \
              "FROM ( " \
              "SELECT  year(start_time) as year , if(birth_year LIKE'%N%',NULL, (start_time)-birth_year)  as user_age_at_trip, birth_year, CAST(gender as SIGNED) as gender " \
     f"FROM {table_name})tab " \
              "GROUP BY year, user_age_at_trip, gender " \
-    # "ORDER BY year, user_age_at_trip desc, gender;"
+
 
 df_user_test = pd.read_sql(query_user, mysql_engine)
 
@@ -63,39 +64,42 @@ df_user_agg = pd.read_csv(agg_data_dir + "user_data.csv")
 
 df_user_agg.fillna(-1, inplace=True)
 df_user_test.fillna(-1, inplace=True)
-#df_user_test.astype({"gender":"int64"}).dtypes
-#pd.to_numeric(df_user_test["gender"])
 
 df_user_agg.sort_values(by=["year", "user_age_at_trip", "gender"], ascending=[True, False, True], inplace=True)
 df_user_test.sort_values(by=["year", "user_age_at_trip", "gender"], ascending=[True, False, True], inplace=True)
-print ("YOUPI", df_user_test.equals(df_user_agg))
-#print(pd.concat([df_user_test, df_user_agg]).drop_duplicates(keep=False).head())
-print(pd.concat([df_user_test, df_user_agg]).drop_duplicates(keep=False).shape)
-if(pd.concat([df_user_test, df_user_agg]).drop_duplicates(keep=False).shape)[0]==0:
+
+if (pd.concat([df_user_test, df_user_agg]).drop_duplicates(keep=False).shape)[0] == 0:
     print("TEST USERS INSIGHTS: OK ")
 else:
-    pd.concat([df_user_test.add_prefix("TEST_"),df_user_agg.add_prefix("SCRIPT_")], axis=1).to_csv(agg_data_dir+"TEST_doublons.csv", index=False)
+    print("TEST USERS INSIGHTS: KO. See log file ")
+    pd.concat([df_user_test.add_prefix("TEST_"), df_user_agg.add_prefix("SCRIPT_")], axis=1).to_csv(
+        agg_data_dir + "TEST_doublons.csv", index=False)
 """
 
-print("df_user_test.dtypes")
-print(df_user_test.dtypes)
-print("df_user_agg.dtypes")
-print(df_user_agg.dtypes)
+
+query_geo="SELECT year, start_station_name as name_station,  lat_start_station, long_start_station, count(1) number_trips "\
+    "FROM(SELECT   year(start_time) as year , start_station_name, cast(start_station_latitude as signed) lat_start_station, cast(start_station_longitude as signed) as long_start_station  "\
+    f"FROM {table_name})tab "\
+    "GROUP BY year, start_station_name, lat_start_station, long_start_station "
+
+df_geo_test=pd.read_sql(query_geo, mysql_engine)
 
 
+df_geo_agg=pd.read_csv(agg_data_dir+"geo_data.csv")
+print("df_geo_agg.dtypes")
+df_geo_agg.astype({"lat_start_station":"object", "long_start_station":"float"}).dtypes
+print(df_geo_agg.dtypes)
 
+print("df_geo_test.dtypes")
+#df_geo_test=df_geo_test.astype({"lat_start_station":"object", "long_start_station":"object"}, copy=True).dtypes
 
+#df_geo_test.astype({"lat_start_station":"float", "long_start_station":"float"}).dtypes
+#df_geo_agg.sort_values(by=["year", "name_station", "lat_start_station", "long_start_station"], ascending=[True, False, True, True], inplace=True)
+#df_geo_test.sort_values(by=["year", "name_station", "lat_start_station", "long_start_station"], ascending=[True, False, True, True], inplace=True)
+#df_geo_test.sort_values(by=["year", "name_station", "lat_start_station", "long_start_station"], ascending=[True, False, True, True], inplace=True)
 
+print(df_geo_test.equals(df_geo_agg))
 
-df_user_agg.reset_index( inplace=True, drop=True)
-
-
-#pd.concat([df_user_test,df_user_agg], axis=1).to_csv(agg_data_dir+"TEST_doublons.csv", index=False)
-test_agg= df_user_agg
-test_agg["user_age_at_trip"]=test_agg["user_age_at_trip"].fillna(-1)
-print(df_user_agg.head())
-print(test_agg.head())
-print(pd.concat([df_user_test,test_agg ]).drop_duplicates(keep=False).shape)
-"""
-
+print(df_geo_test)
+print(pd.concat([df_geo_test, df_geo_agg]).drop_duplicates(keep=False).shape)
 print("PROCESS ENDS")
